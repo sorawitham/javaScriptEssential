08b7eaa9507af1f76677753f451f8afe42d0a924
sorawitham <sorawit8113@gmail.com>
2025-01-20 17:36:04 +0700

    update code scope

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,32 +1,43 @@
-// Global scope
-var globalVar = "I'm a global variable";
-let globalLet = "I'm also global, but scoped with let";
-const globalConst = "I'm a global constant";
+// // Global scope
+// var globalVar = "I'm a global variable";
+// let globalLet = "I'm also global, but scoped with let";
+// const globalConst = "I'm a global constant";
 
 
-{
-    // Block scope
-    var blockVar = "I'm a block-scoped var";
-    let blockLet = "I'm a block-scoped let";
-    const blockConst = "I'm a block-scoped const";
-}
+// {
+//     // Block scope
+//     var blockVar = "I'm a block-scoped var";
+//     let blockLet = "I'm a block-scoped let";
+//     const blockConst = "I'm a block-scoped const";
+// }
 
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+// // Global scope
+// console.log(globalVar); // Output: "I'm a global variable"
+// console.log(globalLet); // Output: "I'm also global, but scoped with let"
+// console.log(globalConst); // Output: "I'm a global constant"
 
-// //Block Scope
+// // //Block Scope
 // console.log(blockVar);
 // console.log(blockLet);
 
-function show() {
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-}
-show();
+// function show() {
+//     var functionVar = "I'm a block-scoped var";
+//     let functionLet = "I'm a block-scoped let";
+//     const functionConst = "I'm a block-scoped const";
+// }
+// show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
+
+{
+    let a = "a outside"
+
+    {
+        let a = "a inside"
+        console.log("Print inside - " + a)
+    }
+    
+    console.log("Print outside - " + a)
+}
