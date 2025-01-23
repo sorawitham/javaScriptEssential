e53364f28de74862c88667d57fe933b76d456813
sorawitham <sorawit8113@gmail.com>
2025-01-23 10:52:26 +0700

    add experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,14 +6,17 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
-    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userProductChoiceFeedback').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userProductChoiceExperience').innerHTML = productType;
+    document.getElementById('userExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 
     alert('Thank you for your valuable feedback');
