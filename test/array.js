0e50e80c734666b112657abceb2479a1f7022cf5
sorawitham <sorawit8113@gmail.com>
2025-01-24 10:26:27 +0700

    add restaurant menu app add testing

---
+++ test/array.js
@@ -0,0 +1,8 @@
+const fruits = ["banana", "cherry"]
+console.log(`Create array: ${fruits}`)
+
+fruits[1] = "mango"
+console.log(`Update array element: ${fruits}`)
+
+fruits[2] = "orange"
+console.log(`Add array element: ${fruits}`)
