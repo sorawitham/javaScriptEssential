e264a762140d1e353e3d6f44f14a51983d09b4e6
sorawitham <sorawit8113@gmail.com>
2025-01-24 10:32:02 +0700

    add restaurant menu app

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,18 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $30', 'Pasta- $25', 'Burger- $25', 'Salmon- $35'];
+const dessertMenu = ['Cake- $10.2', 'Ice Cream- $10.99', 'Pudding- $12.5', 'Fruit Salad- $18'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
